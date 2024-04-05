4c40285b94a7598764188a5791a14e8cf3000a64
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-05 02:12:14 -0300

    add freio service

---
+++ src/main/java/br/glacks/service/FreioService.java
@@ -0,0 +1,19 @@
+package br.glacks.service;
+
+import br.glacks.dto.FreioDTO;
+import br.glacks.dto.ProdutoDTO;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+public interface FreioService {
+
+    public Response getAll();
+
+    public Response getId(@PathParam("id") Long id);
+
+    public Response insert(FreioDTO f);
+
+    public Response update(@PathParam("id") Long id, FreioDTO f);
+
+    public Response delete(@PathParam("id") Long id);
+}
