e24d0d3358d1d9f4e8d4a4301000c4f53874092a
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-05 02:11:44 -0300

    add Categoria service

---
+++ src/main/java/br/glacks/service/CategoriaService.java
@@ -0,0 +1,19 @@
+package br.glacks.service;
+
+import br.glacks.dto.BikeDTO;
+import br.glacks.dto.CategoriaDTO;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+public interface CategoriaService {
+
+    public Response getAll();
+
+    public Response getId(@PathParam("id") Long id);
+
+    public Response insert(CategoriaDTO c);
+
+    public Response update(@PathParam("id") Long id, CategoriaDTO c);
+
+    public Response delete(@PathParam("id") Long id);
+}
