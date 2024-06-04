a7ab2fc0c84236449468fab24a815d37bc7aec5a
kaiomaced0 <kaiomm2000@gmail.com>
2024-06-03 22:58:33 -0300

    carrossel atualizado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -132,7 +132,7 @@ insert into produto_categoria (produto_id, categorias_id) VALUES (106, 6);
  insert into homeconfig (id, ativo) values (1, true);
 
  insert into carrossel (nome, ativo, lista_carrossel, id, link, image) values ('Promoção 1', true, 1, 1, '', 'https://firebasestorage.googleapis.com/v0/b/bike-shop-830cf.appspot.com/o/2024_24_a_31_maio_Orgulho_Nerd_1920x450.jpg?alt=media&token=c96b7dab-cdf4-4fa6-a0e5-d1e9c7c8f25b');
- insert into carrossel (nome, ativo, lista_carrossel, id, link, image) values ('Promoção 2', true, 1, 2, '', 'https://placehold.co/1150x300/black/gray?text=Promoção+2');
+ insert into carrossel (nome, ativo, lista_carrossel, id, link, image) values ('Promoção 2', true, 1, 2, '', 'https://firebasestorage.googleapis.com/v0/b/bike-shop-830cf.appspot.com/o/carrossel%20promocao.png?alt=media&token=64a3a45b-9eba-4d15-adc9-ffcdaf3d29c8');
  insert into carrossel (nome, ativo, lista_carrossel, id, link, image) values ('Promoção 3', true, 1, 3, '', 'https://placehold.co/1150x300/gray/white?text=Promoção+3');
 
  ------------------------------
