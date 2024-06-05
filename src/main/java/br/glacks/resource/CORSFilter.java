e05a022b73eea7147a11f767e52c83ba8cb38183
05562849259 <kaiomm2000@gmail.com>
2024-06-05 18:33:08 -0300

    .

--- src/main/java/br/glacks/resource/CORSFilter.java
+++ src/main/java/br/glacks/resource/CORSFilter.java
@@ -13,7 +13,10 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, PATCH");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, Origin");
+        responseContext.getHeaders().add("Access-Control-Expose-Headers", "Authorization, Content-Disposition");
+
     }
 }
